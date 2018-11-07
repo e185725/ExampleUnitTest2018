94f37405ccc5f476e919c695d86260790bc22c6e
Toshiki Shikina <e185725@ie.u-ryukyu.ac.jp>
2018-11-08 00:35:18 +0900

    1st commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ package jp.ac.uryukyu.ie.e185725;
  * Created by tnal on 2016/11/13.
  */
 public class Main {
+    ////////
     public static void main(String[] args){
         Hero hero = new Hero("勇者", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
@@ -12,7 +13,7 @@ public class Main {
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while( hero.isDead() == false && enemy.isDead() == false ){
+        while( !hero.isDead()  && !enemy.isDead()  ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
