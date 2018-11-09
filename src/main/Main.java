25b6bd0d3258cd3b3eb088eee7d51798cd82f54b
Toshiki Shikina <e185725@ie.u-ryukyu.ac.jp>
2018-11-09 14:41:04 +0900

    1st commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,23 +2,30 @@
 package jp.ac.uryukyu.ie.e185725;
 
 /**
- * Created by tnal on 2016/11/13.
+ *
  */
 public class Main {
     ////////
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
+        Hero hero = new Hero("勇者", 2, 5);
+        Enemy enemy = new Enemy("スライム", 10, 2);
+        LivingThing lt = new LivingThing();
+        lt.getName();
+
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while( !hero.isDead()  && !enemy.isDead()  ){
-            turn++;
-            System.out.printf("%dターン目開始！\n", turn);
-            hero.attack(enemy);
-            enemy.attack(hero);
+        if (lt.getName() == null) {
+
+            while (!hero.isDead() && !enemy.isDead()) {
+
+                turn++;
+                System.out.printf("%dターン目開始！\n", turn);
+                hero.attack(enemy);
+                enemy.attack(hero);
+            }
+            System.out.println("戦闘終了");
         }
-        System.out.println("戦闘終了");
     }
 }
